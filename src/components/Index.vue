a42a7ef96d17094284190b51c8e7b5f0371db638
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2018-07-12 23:02:47 +0900

    Add post card

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,44 @@
 <template>
   <div>
-    hogehoge
+    <v-card>
+      <v-container
+        fluid
+        grid-list-lg
+      >
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card color="grey lighten-5" to="/vuetify">
+              <v-container fluid grid-list-lg>
+                <v-layout row>
+                  <v-flex xs7>
+                    <div>
+                      <div class="headline">About Vuetify</div>
+                      <div>2018-07-10</div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs5>
+                    <v-card-media
+                      :src="image_src"
+                      height="125px"
+                      contain
+                    ></v-card-media>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        image_src: require("@/assets/logo.png")
+      }
+    }
+  }
+</script>
--- src/components/PostVuetify.vue
+++ src/components/PostVuetify.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    This blog written about Vuetify!!
-  </div>
+  <v-container grid-list-md text-xs-left>
+    <div class="subheading">2018-07-10</div>
+    <div class="display-2">About Vuetify</div>
+    <div class="mt-3">
+      This blog written about Vuetify!!
+    </div>
+  </v-container>
 </template>
